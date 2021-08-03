66ba842480b91db97eb7ef5569cc10eaac8b7c05
Suter Yannick <Yannick.Suter@coop.ch>
2021-08-03 23:42:11 +0200

    WIP: Get initial summoner name
    Requires that in the next step an SQL server has to be created

---
+++ src/get_initial_summoner_id.py
@@ -0,0 +1,5 @@
+'''
+This script retrieves the initial summoner id necessary and writes it into the corresponding SQL table.
+Should be executed manually once in the beginning of the project or if the SQL-table has been
+deleted.
+'''
